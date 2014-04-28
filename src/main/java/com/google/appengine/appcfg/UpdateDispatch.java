784be8cd293abaefc8d48f6666974a1338fef478
ludoch <ludovic.champenois@gmail.com>
2014-04-27 19:16:05 -0700

    Update dispatch command supported

---
+++ src/main/java/com/google/appengine/appcfg/UpdateDispatch.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ */
+package com.google.appengine.appcfg;
+
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+
+/**
+ * Update application dispatch.xml.
+ *
+ * @author Ludovic Champenois <ludo@google.com>
+ * @goal update_dispatch
+ * @execute phase="package"
+ */
+public class UpdateDispatch extends AbstractAppCfgMojo {
+
+  @Override
+  public void execute() throws MojoExecutionException, MojoFailureException {
+    getLog().info("");
+    getLog().info("Google App Engine Java SDK - Updating Application Dipatch");
+    getLog().info("");
+
+    getLog().info("Retrieving Google App Engine Java SDK from Maven");
+
+    resolveAndSetSdkRoot();
+
+    String appDir = project.getBuild().getDirectory() + "/" + project.getBuild().getFinalName();
+
+    getLog().info("Updating Dispathc for Google App Engine Application");
+
+    executeAppCfgCommand("update_dispatch", appDir);
+  }
+
+}
