bf466bedd48f1926118bdc4223d55d57611141bc
ludoch <ludovic.champenois@gmail.com>
2014-10-25 19:47:57 -0700

    Adding deploy CLI parameters

--- src/main/java/com/google/appengine/gcloudapp/GCloudAppDeploy.java
+++ src/main/java/com/google/appengine/gcloudapp/GCloudAppDeploy.java
@@ -25,46 +25,91 @@ public class GCloudAppDeploy extends AbstractGcloudMojo {
    *
    * @parameter expression="${appengine.server}"
    */
-  protected String server;
+  private String server;
 
   /**
-   * version The version of the app that will be created or replaced by this deployment.
+   * version The version of the app that will be created or replaced by this
+   * deployment.
    *
    * @parameter expression="${appengine.gcloud_app_version}"
    */
-  protected String gcloud_app_version;
+  private String gcloud_app_version;
 
-    /**
+  /**
    * env-vars ENV_VARS Environment variable overrides for your app.
    *
    * @parameter expression="${appengine.gcloud_app_env_vars}"
    */
-  protected String gcloud_app_env_vars;
-  
-   /**
+  private String gcloud_app_env_vars;
+
+  /**
    * server The App Engine server to connect to.
    *
    * @parameter expression="${appengine.gcloud_app_server}"
    */
-  protected String gcloud_app_server;
-  
-   /**
-   * force Force deploying, overriding any previous in-progress deployments to this version.
+  private String gcloud_app_server;
+
+  /**
+   * force Force deploying, overriding any previous in-progress deployments to
+   * this version.
    *
    * @parameter expression="${appengine.gcloud_app_force}"
    */
-  protected boolean gcloud_app_force;
-  
-
+  private boolean gcloud_app_force;
   /**
-   * Override the default verbosity for this command. 
-   * This must be a standard logging verbosity level: [debug, info,
-   *  warning, error, critical, none] (Default: [warning]).
+   * Set the encoding to be used when compiling Java source files (default
+   * "UTF-8")
    *
-   * @parameter expression="${appengine.gcloud_verbosity}"
+   * @parameter expression="${appengine.gcloud_app_compile_encoding}"
    */
-  protected String gcloud_verbosity;
-  
+  private String gcloud_app_compile_encoding;
+  /**
+   * Delete the JSP source files after compilation
+   *
+   * @parameter expression="${appengine.gcloud_app_delete_jsps}"
+   */
+  private boolean gcloud_app_delete_jsps;
+  /**
+   * Do not jar the classes generated from JSPs
+   *
+   * @parameter expression="${appengine.gcloud_app_disable_jar_jsps}"
+   */
+  private boolean gcloud_app_disable_jar_jsps;
+  /**
+   * Jar the WEB-INF/classes content
+   *
+   * @parameter expression="${appengine.gcloud_app_enable_jar_classes}"
+   */
+  private boolean gcloud_app_enable_jar_classes;
+  /**
+   * Split large jar files (> 32M) into smaller fragments
+   *
+   * @parameter expression="${appengine.gcloud_app_enable_jar_splitting}"
+   */
+  private boolean gcloud_app_enable_jar_splitting;
+  /**
+   * Do not use symbolic links when making the temporary (staging) directory
+   * used in uploading Java apps
+   *
+   * @parameter expression="${appengine.gcloud_app_no_symlinks}"
+   */
+  private boolean gcloud_app_no_symlinks;
+  /**
+   * Do not delete temporary (staging) directory used in uploading Java apps
+   *
+   * @parameter expression="${appengine.gcloud_app_retain_upload_dir}"
+   */
+  private boolean gcloud_app_retain_upload_dir;
+  /**
+   * When --enable-jar-splitting is specified and --jar-splitting-excludes
+   * specifies a comma-separated list of suffixes, a file in a jar whose name
+   * ends with one of the suffixes will not be included in the split jar
+   * fragments
+   *
+   * @parameter expression="${appengine.gcloud_app_jar_splitting_excludes}"
+   */
+  private String gcloud_app_jar_splitting_excludes;
+
   /**
    * @parameter expression="${project}"
    * @required
@@ -94,7 +139,6 @@ public class GCloudAppDeploy extends AbstractGcloudMojo {
 
     ArrayList<String> devAppServerCommand = new ArrayList<>();
     setupInitialCommands(devAppServerCommand);
-    
 
     devAppServerCommand.add("deploy");
 
@@ -113,7 +157,6 @@ public class GCloudAppDeploy extends AbstractGcloudMojo {
     setupExtraCommands(devAppServerCommand);
 
     // Add in additional options for starting the DevAppServer
-
     if (gcloud_app_version != null) {
       devAppServerCommand.add("--version=" + gcloud_app_version);
     }
@@ -124,10 +167,34 @@ public class GCloudAppDeploy extends AbstractGcloudMojo {
       devAppServerCommand.add("--server=" + gcloud_app_server);
     } else if (server != null) {
       devAppServerCommand.add("--server=" + server);
-    }  
+    }
     if (gcloud_app_force) {
-      devAppServerCommand.add("--force" );
-    } 
+      devAppServerCommand.add("--force");
+    }
+    if (gcloud_app_delete_jsps) {
+      devAppServerCommand.add("--delete-jsps");
+    }
+    if (gcloud_app_disable_jar_jsps) {
+      devAppServerCommand.add("--disable-jar-jsps");
+    }
+    if (gcloud_app_enable_jar_classes) {
+      devAppServerCommand.add("--enable-jar-classes");
+    }
+    if (gcloud_app_enable_jar_splitting) {
+      devAppServerCommand.add("--enable-jar-splitting");
+    }
+    if (gcloud_app_compile_encoding != null) {
+      devAppServerCommand.add("--compile-encoding=" + gcloud_app_compile_encoding);
+    }
+    if (gcloud_app_retain_upload_dir) {
+      devAppServerCommand.add("--retain-upload-dir");
+    }
+    if (gcloud_app_no_symlinks) {
+      devAppServerCommand.add("--no-symlinks");
+    }
+    if (gcloud_app_jar_splitting_excludes != null) {
+      devAppServerCommand.add("--jar-splitting-excludes=" + gcloud_app_jar_splitting_excludes);
+    }
     return devAppServerCommand;
   }
 
