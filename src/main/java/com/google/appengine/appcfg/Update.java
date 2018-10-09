e8ae0a3e8c7aa3235cfd51df4d7e2915e698d87a
ludo <ludo@10.1.104.39>
2018-10-09 11:43:48 -0700

    Experiment: stop using appcfg path for new apps..

--- src/main/java/com/google/appengine/appcfg/Update.java
+++ src/main/java/com/google/appengine/appcfg/Update.java
@@ -5,6 +5,11 @@ package com.google.appengine.appcfg;
 
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
+import com.google.appengine.gcloudapp.GCloudAppDeploy;
+import com.google.apphosting.utils.config.AppEngineWebXml;
+import com.google.apphosting.utils.config.AppEngineWebXmlReader;
+import java.io.File;
+import org.apache.maven.project.MavenProject;
 
 /**
  * Create or update an app version.
@@ -25,10 +30,37 @@ public class Update extends AbstractAppCfgMojo {
 
     resolveAndSetSdkRoot();
 
-    String appDir = project.getBuild().getDirectory() + "/" + project.getBuild().getFinalName();
-
     getLog().info("Updating Google App Engine Application");
+    String appDir = getAppDir();
+    Boolean useCloud = false;
+    File f = new File(appDir, "WEB-INF/appengine-web.xml");
+    if (f.exists()) {
+      AppEngineWebXmlReader aewebReader = new AppEngineWebXmlReader(appDir);
+      AppEngineWebXml appEngineWebXml = aewebReader.readAppEngineWebXml();
+      useCloud = "java11".equals(appEngineWebXml.getRuntime());
+    }
 
-    executeAppCfgCommand("update", appDir);
+    if (useCloud) {
+      CloudDeploy deploy = new CloudDeploy(mavenProject, getProjectId(), this.version);
+      deploy.execute();
+    } else {
+      executeAppCfgCommand("update", getAppDir());
+    }
+  }
+
+  private class CloudDeploy extends GCloudAppDeploy {
+
+    CloudDeploy(MavenProject project, String app, String version) {
+      this.maven_project = project;
+      this.staging_directory = project.getBuild().getDirectory() + "/appengine-staging";
+      this.docker_host = "localhost";
+      this.gcloud_project = app;
+      this.version = version;
+    }
+
+    @Override
+    public void resolveAndSetSdkRoot() {
+
+    }
   }
 }
